bb1993d9883693404c56f73ed16e22fc203c348d
Pavels Aleksejevs <paul.alexeev@yahoo.com>
2023-10-03 23:20:25 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,252 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const gameParticipants = [
+  {
+    name: "Pavels Aleksejevs",
+    score: 4,
+    id: 1,
+    favouriteTeam: "Chelsea FC",
+    selectedTeams: [],
+  },
+  {
+    name: "Andy Keaveney",
+    score: 8,
+    id: 2,
+    favouriteTeam: "Chelsea FC",
+    selectedTeams: [],
+  },
+  {
+    name: "Martin Stevenson",
+    score: 7,
+    id: 3,
+    favouriteTeam: "Manchester United",
+    selectedTeams: [],
+  },
+];
+
+function Button({ onClick, children }) {
+  return (
+    <button className="btn" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default function App() {
+  const [participants, setParticipants] = useState(gameParticipants);
+  const [selectTeam, setSelectTeam] = useState(false);
+  const [selectedParticipant, setSelectedParticipant] = useState(null);
+  const [team, setTeam] = useState("");
+
+  console.log(selectedParticipant?.selectedTeams);
+
+  function showFormSelectTeam() {
+    setSelectTeam((show) => !show);
+  }
+
+  function hideFormSelectTeam() {
+    setSelectTeam(false);
+  }
+
+  function handleSelection(participant) {
+    console.log(participant);
+    showFormSelectTeam();
+    setSelectedParticipant(participant);
+  }
+
+  function handleSubmitForm(e) {
+    e.preventDefault();
+    console.log(team);
+    hideFormSelectTeam();
+    selectedParticipant.selectedTeams.push(team);
+    console.log(selectedParticipant);
+    console.log(participants);
+  }
+
+  function teamSelection(e) {
+    setTeam(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParticipantList
+        participants={participants}
+        onShowFormSelectTeam={showFormSelectTeam}
+        onHandleSelection={handleSelection}
+      />
+      {selectTeam && (
+        <FormSelectTeam
+          onHideFormSelectTeam={hideFormSelectTeam}
+          onHandleSubmitForm={handleSubmitForm}
+          teamSelection={teamSelection}
+        />
+      )}
     </div>
   );
 }
 
-export default App;
+function ParticipantList({
+  participants,
+  onShowFormSelectTeam,
+  onHandleSelection,
+}) {
+  return (
+    <ul className="participant-list">
+      {participants.map((participant) => (
+        <Participant
+          participant={participant}
+          key={participant.id}
+          score={participant.score}
+          onShowFormSelectTeam={onShowFormSelectTeam}
+          onHandleSelection={onHandleSelection}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function Participant({
+  participant,
+  score,
+  onShowFormSelectTeam,
+  onHandleSelection,
+}) {
+  const [points1, setPoints1] = useState(score);
+
+  function handlePoints() {
+    setPoints1(() => points1 + 1);
+  }
+
+  return (
+    <li
+      className={`participant ${
+        participant.favouriteTeam === "Chelsea FC"
+          ? "chelsea-background"
+          : "manutd-background"
+      }`}
+      key={participant.id}
+    >
+      <div className="participant-block" onClick={handlePoints}>
+        <h3>{participant.name}</h3>
+        <p className="points">{points1}</p>
+      </div>
+      <Button onClick={() => onHandleSelection(participant)}>Pick Team</Button>
+      <p>Following gameweek team: X</p>
+    </li>
+  );
+}
+
+function FormSelectTeam({ onHandleSubmitForm, teamSelection }) {
+  const [league, setLeague] = useState("");
+  // const [team, setTeam] = useState("");
+
+  console.log(league);
+
+  const premierLeagueTeams = [
+    "Arsenal",
+    "Aston Villa",
+    "Bournemouth",
+    "Brentford",
+    "Brighton",
+    "Burnley",
+    "Chelsea",
+    "Crystal Palace",
+    "Everton",
+    "Fulham",
+    "Liverpool",
+    "Luton Town",
+    "Manchester City",
+    "Manchester United",
+    "Newcastle United",
+    "Nottingham Forest",
+    "Sheffield United",
+    "Tottenham Hotspur",
+    "West Ham United",
+    "Wolverhampton",
+  ];
+
+  const championshipTeams = [
+    "Birmingham City",
+    "Blackburn Rovers",
+    "Bristol City",
+    "Cardiff City",
+    "Coventry City",
+    "Huddersfield Town",
+    "Hull City",
+    "Ipswich Town",
+    "Leeds United",
+    "Leicester City",
+    "Middlesbrough",
+    "Millwall",
+    "Norwich City",
+    "Plymouth Argyle",
+    "Preston North End",
+    "Queens Park Rangers",
+    "Rotherham United",
+    "Sheffield Wednesday",
+    "Southampton",
+    "Stoke City",
+    "Sunderland",
+    "Swansea City",
+    "Watford",
+    "West Bromwich Albion",
+  ];
+
+  return (
+    <div className="team-selection">
+      <form className="team-selection__form" onSubmit={onHandleSubmitForm}>
+        <div>
+          <label>Select league</label>
+          <select value={league} onChange={(e) => setLeague(e.target.value)}>
+            <option disabled value="">
+              -- League --
+            </option>
+            <option value="premierLeague">Premier League</option>
+            <option value="championship">Championship</option>
+          </select>
+        </div>
+
+        {league === "premierLeague" && (
+          <div>
+            <select onChange={teamSelection}>
+              <option disabled selected value="">
+                -- Team --
+              </option>
+              {premierLeagueTeams.map((team) => (
+                <option
+                // value={
+                //   team[0].toLowerCase() +
+                //   team.substring(1).split(" ").join("")
+                // }
+                >
+                  {team}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        {league === "championship" && (
+          <div>
+            <select onChange={teamSelection}>
+              <option disabled selected value>
+                -- Team --
+              </option>
+              {championshipTeams.map((team) => (
+                <option
+                // value={
+                //   team[0].toLowerCase() +
+                //   team.substring(1).split(" ").join("")
+                // }
+                >
+                  {team}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+        <Button>Submit</Button>
+      </form>
+    </div>
+  );
+}
